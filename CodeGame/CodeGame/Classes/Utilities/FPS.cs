d5a473421eb9159acb7b88cb6d76da71cf127921
Derek Smith <dsmith.programmer@gmail.com>
2011-07-09 01:10:20 -0700

    Added FPS counter class for performance tracking. Doesn't seem to work after first attempt.

---
+++ CodeGame/CodeGame/Classes/Utilities/FPS.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.Diagnostics;
+
+namespace CodeGame.Classes.Utilities {
+    class FPS {
+        Stopwatch _watch;
+        public enum Display { TopLeft, TopRight, BottomRight, BottomLeft }
+        SpriteFont _font;
+        Vector2 _vector;
+        long _elapsed;
+        int _counter;
+        int _snapshot;
+        const int _margin = 5;
+
+        public FPS(ContentManager content, Rectangle window, Display display) {
+            _font = content.Load<SpriteFont>(@"Utilities\Tahoma");
+            switch (display) {
+                case Display.TopLeft:
+                    _vector = new Vector2(_margin, _margin);
+                    break;
+                case Display.TopRight:
+                    _vector = new Vector2(window.Width - 30 - _margin, _margin);
+                    break;
+                case Display.BottomRight:
+                    Debug.WriteLine("screen.Width: " + window.Width.ToString());
+                    Debug.WriteLine("screen.Height: " + window.Height.ToString());
+                    _vector = new Vector2(window.Width - 30 - _margin, window.Height - 20 - _margin);
+                    break;
+                case Display.BottomLeft:
+                    _vector = new Vector2(_margin, window.Height - 20 - _margin);
+                    break;
+            }
+            _watch = new Stopwatch();
+            _watch.Start();
+        }
+
+        public void Update() {
+            _elapsed += _watch.ElapsedMilliseconds;
+            if (_elapsed >= 1000) {
+                _snapshot = _counter;
+                _counter = 0;
+                _elapsed -= 1000;
+                _watch.Restart();
+            }
+            else
+                _counter++;
+        }
+
+        public void Draw(SpriteBatch batch) {
+            batch.Begin();
+            batch.DrawString(_font, _snapshot.ToString(), _vector, Color.Red);
+            batch.End();
+        }
+    }
+}
