c6be9be595a28a7bef0a2442714c3d6d7625d690
Derek Smith <dsmith.programmer@gmail.com>
2011-07-08 21:58:39 -0700

    Changed filename.

--- CodeGame/CodeGame/Program.cs
+++ CodeGame/CodeGame/Program.cs
@@ -9,7 +9,7 @@ namespace CodeGame {
         /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
+            using (Game game = new Game())
             {
                 game.Run();
             }
